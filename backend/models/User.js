13ed3d60d9024a79b9c1aa20ada84a3c513d891e
Kaveesha Subashana <kaveeshasubashana@gmail.com>
2025-12-06 15:38:04 +0530

    backend user model

---
+++ backend/models/User.js
@@ -0,0 +1,51 @@
+// backend/models/User.js
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please add a name"],
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: [true, "Please add an email"],
+      unique: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Please add a password"],
+      minlength: 6,
+    },
+    role: {
+      type: String,
+      enum: ["admin", "supplier", "supermarket"],
+      default: "supermarket",
+    },
+    isApproved: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+// Hash password before save
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+// Compare password
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
